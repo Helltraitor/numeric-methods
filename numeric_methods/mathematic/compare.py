33ac9d58edf744a58fc0f862ac2eaf67d9ef4e63
Helltraitor <helltraitor@hotmail.com>
2022-04-10 22:31:51 +0300

    Added compare function

---
+++ numeric_methods/mathematic/compare.py
@@ -0,0 +1,19 @@
+from decimal import Decimal
+from fractions import Fraction
+from math import isclose
+
+from numeric_methods.mathematic import EPSILON
+
+
+NUMBER = Decimal | float | Fraction
+
+
+def compare(lhs: NUMBER, kind: str, rhs: NUMBER) -> bool:
+    epsilon = EPSILON.with_context(lhs)
+    return {
+        "==": lambda: isclose(lhs, rhs, abs_tol=epsilon),
+        "<=": lambda: (lhs < rhs) or isclose(lhs, rhs, abs_tol=epsilon),
+        ">=": lambda: (lhs > rhs) or isclose(lhs, rhs, abs_tol=epsilon),
+        "<": lambda: lhs < rhs,
+        ">": lambda: lhs > rhs
+    }[kind]()
