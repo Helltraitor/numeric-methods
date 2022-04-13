d7b885b6204fecb0c65efef76cb88c0ffd595aee
Helltraitor <helltraitor@hotmail.com>
2022-04-13 21:49:47 +0300

    Added docs for epsilon settings

--- numeric_methods/language/docs/settings.py
+++ numeric_methods/language/docs/settings.py
@@ -1,3 +1,12 @@
+DEFAULT_EPSILON_DOCS = {
+    "ENGLISH": """
+    :return: Default epsilon value as string
+    """,
+    "RUSSIAN": """
+    :return: Значение по умолчанию в виде строки
+    """
+}
+
 GET_EPSILON_DOCS = {
     "ENGLISH": """
     By using type of passed kind returns the current epsilon value in this type
@@ -47,6 +56,54 @@ GET_LANGUAGE_DOCS = {
         | print(settings.get_language())  # ENGLISH
 
     :return: Строка полного названия языка
+    """
+}
+
+RESTORE_EPSILON_DOCS = {
+    "ENGLISH": """
+    Restores default value of epsilon. Default value can be accessed through `default_epsilon` function
+    """,
+    "RUSSIAN": """
+    Восстанавливает значение эпсилон по умолчанию. Это значение может быть доступно в `default_epsilon`
+    """
+}
+
+SET_EPSILON_DOCS = {
+    "ENGLISH": """
+    Sets a new value for mathematics epsilon value. This value is used for value comparing in project.
+    Changing this value may increase precision of evaluation in whole project. This value is not
+    an epsilon of method functions - they have special epsilon parameters. Don't change it if you
+    not need it
+    Example:
+        | from numeric_methods import mathematics
+        | from numeric_methods import settings
+        |
+        |
+        | print(mathematics.compare(0.0001, "==", 0.001))  # False
+        | if settings.set_epsilon("0.001"):
+        |     # Value 0.001 was sat only for example you can set any value you need
+        |     print(mathematics.compare(0.0001, "==", 0.001))  # True
+
+    :param value: String value of integer, probably in exponential notation
+    :return: True if value was sat otherwise False
+    """,
+    "RUSSIAN": """
+    Устанавливает новое значение эпсилон математического модуля. Это значени используется для сравнения
+    значений в проекте. Изменение данного значение может повысить точность вычислений во всем проекте.
+    Это значение не является эпсилоном функций - те имеют собственные параметры эпсилон. Не изменяйте
+    значение, если это не требуется 
+    Пример:
+        | from numeric_methods import mathematics
+        | from numeric_methods import settings
+        |
+        |
+        | print(mathematics.compare(0.0001, "==", 0.001))  # False
+        | if settings.set_epsilon("0.001"):
+        |     # Значение 0.001 было установлено только в качестве примера, вы можете установить любое
+        |     print(mathematics.compare(0.0001, "==", 0.001))  # True
+
+    :param value: String value of integer, probably in exponential notation
+    :return: True if value was sat otherwise False
     """
 }
 
--- numeric_methods/settings.py
+++ numeric_methods/settings.py
@@ -3,7 +3,10 @@ from fractions import Fraction
 from typing import Type
 
 from numeric_methods.language import LANGUAGE, TRANSLATE
-from numeric_methods.language.docs.settings import GET_LANGUAGE_DOCS, SET_LANGUAGE_DOCS, GET_EPSILON_DOCS
+from numeric_methods.language.docs.settings import (
+    GET_LANGUAGE_DOCS, SET_LANGUAGE_DOCS,
+    DEFAULT_EPSILON_DOCS, GET_EPSILON_DOCS, RESTORE_EPSILON_DOCS, SET_EPSILON_DOCS
+)
 from numeric_methods.mathematics import EPSILON
 
 NUMBER = Decimal | float | Fraction
@@ -24,14 +27,21 @@ def set_language(name: str) -> bool:
     return False
 
 
+@TRANSLATE.documentation(DEFAULT_EPSILON_DOCS)
+def default_epsilon() -> str:
+    return EPSILON.get_default()
+
+
 @TRANSLATE.documentation(GET_EPSILON_DOCS)
 def get_epsilon(kind: Type[NUMBER] = float) -> NUMBER:
     return EPSILON.with_context(kind(0) if callable(kind) else kind)
 
 
-def set_epsilon(value: str) -> bool:
-    return EPSILON.set(value)
-
-
+@TRANSLATE.documentation(RESTORE_EPSILON_DOCS)
 def restore_epsilon():
     EPSILON.restore()
+
+
+@TRANSLATE.documentation(SET_EPSILON_DOCS)
+def set_epsilon(value: str) -> bool:
+    return EPSILON.set(value)
