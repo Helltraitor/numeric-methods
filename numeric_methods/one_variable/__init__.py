9eba7c2a1d9e7005be7547b5f08d443820061809
Helltraitor <helltraitor@hotmail.com>
2022-05-21 19:48:46 +0300

    Added tangent method

--- numeric_methods/one_variable/__init__.py
+++ numeric_methods/one_variable/__init__.py
@@ -1,7 +1,9 @@
 from .half_method import half_method
 from .iter_method import iter_method
+from .tangent_method import tangent_method
 
 __all__ = [
     half_method,
-    iter_method
+    iter_method,
+    tangent_method
 ]
