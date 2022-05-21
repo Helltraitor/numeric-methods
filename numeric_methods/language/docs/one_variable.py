3d2aa14ccd2c00b29fb7d2c0823abd87f6426388
Helltraitor <helltraitor@hotmail.com>
2022-05-21 19:48:58 +0300

    Added tangent method docs

--- numeric_methods/language/docs/one_variable.py
+++ numeric_methods/language/docs/one_variable.py
@@ -126,7 +126,7 @@ ITER_METHOD_DOCS = {
         STOP
 
     Условия использования:
-        [Вручную] Должно быть истинной или будет вызвана ArithmeticError will или алгоритм будет исполнятся вечно: |function'(x_(i))| < 1
+        [Вручную] Должно быть истинной или будет вызвана ArithmeticError или алгоритм будет исполнятся вечно: |function'(x_(i))| < 1
 
     Пример:
         | from math import sin
@@ -144,4 +144,77 @@ ITER_METHOD_DOCS = {
     :param c_criterion: Критерий сходимости
     :return: Корень уравнения `function(x) = x` с указанной точностью
     """,
+}
+
+TANGENT_METHOD_DOCS = {
+    "ENGLISH": """
+    This method is implementing the tangent lines' method (Newton's method) of root searching.
+
+    Algorithm:
+        x_(i + 1) = x_(i) - function(x_(i)) / differential(x_(i))
+        return step, x_(i + 1)
+        while not |x_(i + 1) - x_(i)| < epsilon:
+            x_(i) = x_(i + 1)
+            x_(i + 1) = x_(i) - function(x_(i)) / differential(x_(i))
+            return step, x_(i + 1)
+        return x_(i + 1)
+        STOP
+
+    Conditions of using:
+        [Automatic] Must be truth or ValueError will raise: x or double_differential must be specified
+        [Automatic] Must be truth or ArithmeticError will raise: function(a) * function(b) < 0
+        [Manually] Must be truth or ArithmeticError will raise or algorithm will execute forever: function is double differentiable continuously in [a_0, b_0]
+
+    Example:
+        | from numeric_methods.one_variable import tangent_method
+        |
+        |
+        | # x ** 3 = 7
+        | for line in tangent_method(lambda x: x**3 - 7, lambda x: 3*x**2, 1, 2, 0.001, double_differential=lambda x: 6*x):
+        |     print(line)
+
+    :param function: Lambda or defined function which must support type of number and be continuity
+    :param differential: Differential of function that must be continuity
+    :param a: Begin of the slice where root is
+    :param b: End of the slice where root is
+    :param epsilon: Required precision of the `function(x) = 0` root
+    :param x: Start value of x (x_0)
+    :param double_differential: Double differential of function that must be continuity
+    :return: Root of the `function(x) = 0` with indicated precision
+    """,
+    "RUSSIAN": """
+    Данный метод реализует нахождение корня методом касательных (методом Ньютона).
+
+    Алгоритм:
+        x_(i + 1) = x_(i) - function(x_(i)) / differential(x_(i))
+        return step, x_(i + 1)
+        while not |x_(i + 1) - x_(i)| < epsilon:
+            x_(i) = x_(i + 1)
+            x_(i + 1) = x_(i) - function(x_(i)) / differential(x_(i))
+            return step, x_(i + 1)
+        return x_(i + 1)
+        STOP
+
+    Условия использования:
+        [Автоматически] Должно быть истинной или будет вызвана ValueError: x или double_differential должно быть указано
+        [Автоматически] Должно быть истинной или будет вызвана ArithmeticError: function(a) * function(b) < 0
+        [Вручную] Должно быть истинной или будет вызвана ArithmeticError или алгоритм будет исполнятся вечно: функция должна быть дважды непрерывно дифференцируема на [a, b]
+
+    Пример:
+        | from numeric_methods.one_variable import tangent_method
+        |
+        |
+        | # x ** 3 = 7
+        | for line in tangent_method(lambda x: x**3 - 7, lambda x: 3*x**2, 1, 2, 0.001, double_differential=lambda x: 6*x):
+        |     print(line)
+
+    :param function: Лямбда-функция или заранее определенная функция, которая должна поддерживать данный тип числа и быть непрерывной
+    :param differential: Дифференциал функции, который должен быть непрерывным
+    :param a: Начало отрезка, в котором находится корень
+    :param b: Конец отрезка, в котором находится корень
+    :param epsilon: Требуемая точность корня уравнения `function(x) = 0`
+    :param x: Начальное значение x (x_0)
+    :param double_differential: Двойной дифференциал функции, который должен быть непрерывным
+    :return: Корень уравнения `function(x) = 0` с указанной точностью
+    """
 }
